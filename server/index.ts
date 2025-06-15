6c7afbed5cc51e9f0f0cfb555a4a3548e70f8e32
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-06-16 00:15:55 +0800

    💫 init: server infrastructure

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import { loadEnv } from "@/config/loadEnv";
+import { connectToDb } from "@/infrastructure/database/connectToDb";
+import { startApp } from "@/infrastructure/express/app";
+
+const initializeServer = async () => {
+  try {
+    loadEnv();
+
+    await connectToDb();
+
+    const app = startApp();
+
+    const port = parseInt(process.env.PORT || "3000");
+
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server", error);
+    process.exit(1);
+  }
+};
+
+initializeServer();
---
+++ server/infrastructure/database/connectToDb.ts
@@ -0,0 +1,4 @@
+export const connectToDb = async (): Promise<void> => {
+  //  To connect the database here
+  // try-catch for connection of database
+};
