232e8f81d16a5364bf6e969189e50ad7ed4caab9
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-06-28 21:35:32 +0800

    ✨ feature: auth middleware

--- server/internal/user/route.ts
+++ server/internal/user/route.ts
@@ -1,11 +1,16 @@
+import { protectRoute } from "@/internal/auth/config";
 import { userController } from "@/internal/user/config";
 import express from "express";
 
 const router = express.Router();
 
-router.put("/update", userController.updateUser);
-router.put("/update-profile-picture", userController.updateUserProfilePic);
-router.delete("/delete", userController.deleteUser);
-router.get("/", userController.getAllUsers);
+router.put("/update", protectRoute, userController.updateUser);
+router.put(
+  "/update-profile-picture",
+  protectRoute,
+  userController.updateUserProfilePic
+);
+router.delete("/delete", protectRoute, userController.deleteUser);
+router.get("/", protectRoute, userController.getAllUsers);
 
 export default router;
