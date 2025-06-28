69de0f5e5778b62de99ad459d2f6f67251f1f449
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-06-28 21:01:51 +0800

    🧹 chore: adjust user controller & fix dependency issues

--- server/infrastructure/middleware/interface.ts
+++ server/infrastructure/middleware/interface.ts
@@ -1,3 +1,17 @@
+import { Request } from "express";
+
+export interface SafeUser {
+  id: string;
+  username: string;
+  password?: string;
+  email: string;
+  profilePic: string;
+}
+
+export interface CustomRequest extends Request {
+  user?: SafeUser;
+}
+
 export interface ErrorResponseProps {
   status: "failed" | "error";
   message: string;
--- server/internal/user/route.ts
+++ server/internal/user/route.ts
@@ -1,10 +1,11 @@
+import { userController } from "@/internal/user/config";
 import express from "express";
 
 const router = express.Router();
 
-router.put("/update");
-router.put("/update-profile-picture");
-router.delete("/delete");
-router.get("/");
+router.put("/update", userController.updateUser);
+router.put("/update-profile-picture", userController.updateUserProfilePic);
+router.delete("/delete", userController.deleteUser);
+router.get("/", userController.getAllUsers);
 
 export default router;
