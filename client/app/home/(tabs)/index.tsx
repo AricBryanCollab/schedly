1fd11b7b4bdedf56e9cab7e1f053ec5c0b4f6a00
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-07-11 05:41:37 +0800

    🧩 component: calendar (temporary)

--- client/app/home/(tabs)/index.tsx
+++ client/app/home/(tabs)/index.tsx
@@ -1,13 +1,53 @@
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
-import React from "react";
-import { Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Calendar } from "react-native-calendars";
 
 const CalendarScreen = () => {
+  const [selected, setSelected] = useState<string>("");
+
   return (
     <ScreenWrapper>
-      <Text>Home Page</Text>
+      <View style={{ flex: 1, marginVertical: 80 }}>
+        <Calendar
+          style={styles.calendar}
+          theme={{
+            backgroundColor: "transparent",
+            calendarBackground: "transparent",
+            todayTextColor: "#337ed3",
+            dayTextColor: "#052347",
+            textDisabledColor: "#ccc",
+            arrowColor: "#337ed3",
+            monthTextColor: "#052347",
+            textMonthFontWeight: "bold",
+            textDayFontWeight: "400",
+            textDayFontSize: 16,
+            textMonthFontSize: 18,
+            textDayHeaderFontSize: 14,
+          }}
+          onDayPress={(day) => {
+            setSelected(day.dateString);
+          }}
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedColor: "#8b8de695",
+              selectedTextColor: "#fff",
+            },
+          }}
+        />
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default CalendarScreen;
+
+const styles = StyleSheet.create({
+  calendar: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    elevation: 0,
+  },
+});
