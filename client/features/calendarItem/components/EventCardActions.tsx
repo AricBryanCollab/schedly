5b12118e5c227005d1a46823350e3d567ed7b1bd
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-08-01 23:43:31 +0800

    🧩 component: confirm delete modal

--- client/features/calendarItem/components/EventCardActions.tsx
+++ client/features/calendarItem/components/EventCardActions.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Divider, IconButton, Menu } from "react-native-paper";
 
+import ConfirmDeleteModal from "@/components/ui/ConfirmDeleteModal";
 import { useRouter } from "expo-router";
 
 interface EventCardActionsProps {
@@ -18,7 +20,13 @@ const EventCardActions = ({
 }: EventCardActionsProps) => {
   const router = useRouter();
 
-  const handleEditePress = () => {
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+
+  const showDeleteModal = () => setIsDeleteModalVisible(true);
+
+  const hideDeleteModal = () => setIsDeleteModalVisible(false);
+
+  const handleEditPress = () => {
     onClose();
     router.push({
       pathname: "/(calendar-item)/[id]/edit-item",
@@ -26,25 +34,51 @@ const EventCardActions = ({
     });
   };
 
+  const handleDeletePress = () => {
+    onClose();
+    showDeleteModal();
+  };
+
+  const handleConfirmDelete = (id: string) => {
+    console.log(id);
+    //Todo: add API call for delete calendar item
+    hideDeleteModal();
+  };
+
   return (
-    <View style={styles.container}>
-      <Menu
-        visible={openMenu}
-        onDismiss={onClose}
-        anchor={
-          <IconButton onPress={onOpen} icon="dots-horizontal" size={28} />
-        }
-        anchorPosition="bottom"
-      >
-        <Menu.Item
-          leadingIcon="pencil"
-          onPress={handleEditePress}
-          title="Edit"
-        />
-        <Divider />
-        <Menu.Item leadingIcon="delete" onPress={() => {}} title="Delete" />
-      </Menu>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Menu
+          visible={openMenu}
+          onDismiss={onClose}
+          anchor={
+            <IconButton onPress={onOpen} icon="dots-horizontal" size={28} />
+          }
+          anchorPosition="bottom"
+        >
+          <Menu.Item
+            leadingIcon="pencil"
+            onPress={handleEditPress}
+            title="Edit"
+          />
+          <Divider />
+          <Menu.Item
+            leadingIcon="delete"
+            onPress={handleDeletePress}
+            title="Delete"
+          />
+        </Menu>
+      </View>
+
+      <ConfirmDeleteModal
+        visible={isDeleteModalVisible}
+        onDismiss={hideDeleteModal}
+        onClose={hideDeleteModal}
+        onDelete={() => handleConfirmDelete(itemId)}
+        title="Delete Event"
+        statement={`Are you sure you want to delete this event with ID ${itemId} ? This action cannot be undone.`}
+      />
+    </>
   );
 };
 
